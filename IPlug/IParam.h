a283d8a4ec6a2db6a4e4b434b52ae397d7c9504b
Theo Niessink <theo@taletn.com>
2020-09-07 16:19:36 +0200

    IParam: Made base parameter class virtual.

--- IPlug/IParam.h
+++ IPlug/IParam.h
@@ -1,52 +1,49 @@
 #pragma once
 
 #include "Containers.h"
-#include <math.h>
 
 #ifndef MAX_PARAM_NAME_LEN
 const int MAX_PARAM_NAME_LEN = 32;
 #endif
 
-inline double ToNormalizedParam(double nonNormalizedValue, double min, double max, double shape)
-{
-  return pow((nonNormalizedValue - min) / (max - min), 1.0 / shape);
-}
-
-inline double FromNormalizedParam(double normalizedValue, double min, double max, double shape)
-{
-  return min + pow((double) normalizedValue, shape) * (max - min);
-}
-
 class IParam
 {
 public:
+	enum EParamType { kTypeNone = 0, kTypeBool, kTypeInt, kTypeEnum, kTypeDouble };
 
-  enum EParamType { kTypeNone, kTypeBool, kTypeInt, kTypeEnum, kTypeDouble };
+	IParam(
+		const int type
+	):
+		mType(type),
+		mNegateDisplay(false)
+	{}
 
-	IParam();
-  ~IParam();
+	virtual ~IParam() {}
 
-  EParamType Type() { return mType; }
-	
+	inline int Type() const { return mType; }
+
 	// Call this if your param is (x, y) but you want to always display (-x, -y).
 	void NegateDisplay() { mNegateDisplay = true; }
 	bool DisplayIsNegated() const { return mNegateDisplay; }
 
-	void SetNormalized(double normalizedValue);
-	double GetNormalized();
-	double GetNormalized(double nonNormalizedValue);
-  void GetDisplayForHost(char* rDisplay) { GetDisplayForHost(mValue, false, rDisplay); }
-  void GetDisplayForHost(double value, bool normalized, char* rDisplay);
+	virtual void SetNormalized(double normalizedValue) = 0;
+	virtual double GetNormalized() const = 0;
+	virtual double GetNormalized(double nonNormalizedValue) const = 0;
+	virtual char* GetDisplayForHost(char* buf, int bufSize = 128) = 0;
+	virtual char* GetDisplayForHost(double normalizedValue, char* buf, int bufSize = 128) = 0;
 	const char* GetNameForHost();
-	const char* GetLabelForHost();
+	virtual const char* GetLabelForHost() const { return ""; }
 
-  int GetNDisplayTexts();
-	bool MapDisplayText(char* str, int* pValue);	// Reverse map back to value.
+	virtual int GetNDisplayTexts() const { return 0; }
 
-private:
+	// Reverse map back to value.
+	virtual bool MapDisplayText(const char* /* str */, double* /* pNormalizedValue */) const
+	{
+		return false;
+	}
 
-  EParamType mType;
-	int mDisplayPrecision;
+protected:
+	char mType, mDisplayPrecision;
 	char mName[MAX_PARAM_NAME_LEN];
 	bool mNegateDisplay;
 };
