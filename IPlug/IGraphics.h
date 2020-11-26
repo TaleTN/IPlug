78bab52b7f65b318734ac27f9d7026475723dc5b
Theo Niessink <theo@taletn.com>
2020-11-26 11:55:18 +0100

    IGraphics: Added return value to host/plugin path.

--- IPlug/IGraphics.h
+++ IPlug/IGraphics.h
@@ -68,8 +68,8 @@ public:
 	enum EPromptFlags { kPromptCustomWidth = 0x80000000, kPromptCustomRect = 0xC0000000 };
 	virtual void PromptUserInput(IControl* pControl, IParam* pParam, const IRECT* pR = NULL, int fontSize = 0) = 0;
 	void SetFromStringAfterPrompt(IControl* pControl, const IParam* pParam, const char* txt);
-	virtual void HostPath(WDL_String* pPath) = 0; // Full path to host executable.
-	virtual void PluginPath(WDL_String* pPath) = 0; // Full path to plugin dll.
+	virtual bool HostPath(WDL_String* pPath) = 0; // Full path to host executable.
+	virtual bool PluginPath(WDL_String* pPath) = 0; // Full path to plugin dll.
 	// Run the "open file" or "save file" dialog; extensions = "txt wav" for example.
 	enum EFileAction { kFileOpen = 0, kFileSave };
 	virtual bool PromptForFile(WDL_String* pFilename, int action = kFileOpen, const char* dir = NULL, const char* extensions = NULL) = 0;
