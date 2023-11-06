TaleTN/IPlug

commit f27e3b6d08e771fe4a6dcaf4df2eaabd41580451

IGraphics: Patched draw text offset on macOS.

IPlug/IGraphics.cpp
@@ -616,7 +616,13 @@ int IGraphics::DrawIText(IText* const pTxt, const char* const str, const IRECT* 
 		R.right >>= scale;
 		R.bottom >>= scale;
 	}
- 
+
+	// TN: Quick patch to fix vertical offset.
+	#ifdef __APPLE__
+	R.top--;
+	R.top = wdl_max(R.top, 0);
+	#endif
+
 	const int h = font->DrawText(mDrawBitmap, str, -1, &R, fmt) << scale;
 
 	return h;

commit 0a54412d0c31ecdbb0f67d90986948ed058a0360

IGraphics: Fixed draw text negative y-coordinate.
Fixes macOS regression introduced in commit f27e3b6d08, oops!

IPlug/IGraphics.cpp
@@ -624,7 +624,6 @@ int IGraphics::DrawIText(IText* const pTxt, const char* const str, const IRECT* 
 	// TN: Quick patch to fix vertical offset.
 	#ifdef __APPLE__
 	R.top--;
-	R.top = wdl_max(R.top, 0);
 	#endif
 
 	const int h = font->DrawText(mDrawBitmap, str, -1, &R, fmt) << scale;