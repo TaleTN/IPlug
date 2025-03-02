TaleTN/IPlug

commit 4479c6cae6ac370e5256379b2c980ec31f84bb63

INormalizedParam: Added normalized parameter type.

IPlug/IParam.h
@@ -13,7 +13,7 @@
 class IParam
 {
 public:
-	enum EParamType { kTypeNone = 0, kTypeBool, kTypeInt, kTypeEnum, kTypeDouble };
+	enum EParamType { kTypeNone = 0, kTypeBool, kTypeInt, kTypeEnum, kTypeDouble, kTypeNormalized };
 
 	IParam(
 		const int type,
@@ -425,5 +425,42 @@ public:
 
 protected:
 	double WDL_FIXALIGN mShape, mExpMin1;
+}
+WDL_FIXALIGN;
+
+class INormalizedParam: public IParam
+{
+public:
+	INormalizedParam(const char* name, double defaultVal = 0.0);
+
+	inline void Set(const double normalizedValue)
+	{
+		assert(normalizedValue >= 0.0 && normalizedValue <= 1.0);
+		mValue = normalizedValue;
+	}
+
+	inline double Value() const { return mValue; }
+
+	static double Bounded(double normalizedValue)
+	{
+		normalizedValue = wdl_max(normalizedValue, 0.0);
+		normalizedValue = wdl_min(normalizedValue, 1.0);
+		return normalizedValue;
+	}
+
+	void SetNormalized(double normalizedValue);
+	double GetNormalized() const { return mValue; }
+	double GetNormalized(double nonNormalizedValue) const;
+	char* GetDisplayForHost(char* buf, int bufSize = 128);
+	char* GetDisplayForHost(double normalizedValue, char* buf, int bufSize = 128);
+
+	char* ToString(double normalizedValue, char* buf, int bufSize = 128) const;
+
+	bool Serialize(ByteChunk* pChunk) const;
+	int Unserialize(const ByteChunk* pChunk, int startPos);
+	int GetSize() const { return (int)sizeof(double); }
+
+protected:
+	double WDL_FIXALIGN mValue;
 }
 WDL_FIXALIGN;

IPlug/IParam.cpp
@@ -556,3 +556,61 @@ double IDoubleExpParam::GetNormalized(const double nonNormalizedValue) const
 {
 	return ToNormalized(Bounded(nonNormalizedValue));
 }
+
+INormalizedParam::INormalizedParam(
+	const char* const name,
+	const double defaultVal
+):
+	IParam(kTypeNormalized, name),
+	mValue(defaultVal)
+{
+	assert(defaultVal >= 0.0 && defaultVal <= 1.0);
+}
+
+void INormalizedParam::SetNormalized(const double normalizedValue)
+{
+	Set(normalizedValue);
+}
+
+double INormalizedParam::GetNormalized(const double nonNormalizedValue) const
+{
+	return Bounded(nonNormalizedValue);
+}
+
+char* INormalizedParam::ToString(const double normalizedValue, char* const buf, const int bufSize) const
+{
+	assert(normalizedValue >= 0.0 && normalizedValue <= 1.0);
+
+	char tmp[12];
+	sprintf(tmp, "%.6g", normalizedValue);
+
+	if (tmp[0] && !tmp[1])
+	{
+		tmp[1] = '.';
+		tmp[2] = '0';
+		tmp[3] = 0;
+	}
+
+	lstrcpyn_safe(buf, tmp, bufSize);
+	return buf;
+}
+
+char* INormalizedParam::GetDisplayForHost(char* const buf, const int bufSize)
+{
+	return ToString(mValue, buf, bufSize);
+}
+
+char* INormalizedParam::GetDisplayForHost(const double normalizedValue, char* const buf, const int bufSize)
+{
+	return ToString(normalizedValue, buf, bufSize);
+}
+
+bool INormalizedParam::Serialize(ByteChunk* const pChunk) const
+{
+	return !!pChunk->PutDouble(mValue);
+}
+
+int INormalizedParam::Unserialize(const ByteChunk* const pChunk, const int startPos)
+{
+	return pChunk->GetDouble(&mValue, startPos);
+}

commit 7e0033bc1b0c5a38022a0fb1490275e50daced01

IPlug: Fixed normalized parameter get size method.
Probably broken since commit 4479c6cae6, oops!

IPlug/IParam.h
@@ -588,7 +588,7 @@ public:
 
 	bool Serialize(ByteChunk* pChunk) const;
 	int Unserialize(const ByteChunk* pChunk, int startPos);
-	int GetSize() const { return (int)sizeof(double); }
+	int Size() const { return (int)sizeof(double); }
 
 protected:
 	double WDL_FIXALIGN mValue;